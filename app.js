d7fbf56bc53db8757bb69589f5ecd1822a8b9a91
Francisco Guerra <paco.gq.0709@gmail.com>
2024-09-07 18:26:15 -0600

    update link

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 // app.use(cors());
 app.use(cors({
-  origin: 'https://postre-client.herokuapp.com',
+  origin: 'https://quiet-leandra-elpostre-a2b95fa0.koyeb.app/',
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH']
 }))
 
