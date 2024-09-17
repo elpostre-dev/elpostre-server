8f39bc8f73cf9e2fbfeb234bfe6a0192a50c70bc
Francisco Guerra <paco.gq.0709@gmail.com>
2024-09-17 15:17:41 -0600

    auth config changes 3

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,11 @@
 const bcrypt = require('bcryptjs');
-const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/users.model');
 const aH = require('express-async-handler');
 const MyError = require('../utils/myError');
-const messages = require('../utils/messages')
-const authServices = require('../services/auth')
+const messages = require('../utils/messages');
+const authServices = require('../services/auth');
+
 exports.signup = aH(async (req, res, next) => {
     const {
         sName, sLastname, sEmail, sPassword
@@ -30,27 +30,25 @@ exports.signup = aH(async (req, res, next) => {
 exports.login = async (req, res, next) => {
     let { email, password } = req.body;
 
-    email = email.toLowerCase()
+    email = email.toLowerCase();
 
-    //   format email input
+    // Format email input
     const user = await User.query().modify('getUserByEmailLogin', email);
     if (!user) {
         console.log('no user');
         return next(new MyError(401, messages.auth.login.invalidCredentials.sp));
     }
-    // const pass = bcrypt.compareSync(password, user.sPassword);
-    console.log('user from DB:', user)
-    console.log('typed pass from User:', password)
-    let typedPass = bcrypt.hashSync(password, 10)
-    console.log('typed pass hashed:', typedPass)
-    console.log('hash pass from DB:', user.sPassword)
-    const pass = (typedPass === user.sPassword)
-    if (!pass) {
+
+    // Compare entered password with hashed password in the database
+    const isPasswordCorrect = bcrypt.compareSync(password, user.sPassword);
+    if (!isPasswordCorrect) {
         console.log('no correct pass');
         return next(new MyError(401, messages.auth.login.invalidCredentials.sp));
     }
 
-    delete user.password;
+    // Remove the password from the user object before sending the response
+    delete user.sPassword;
 
+    // Create a token and send the response
     authServices.createSendToken(user, 200, messages.auth.login.success, res);
 };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,33 +5,32 @@ const messages = require('../utils/messages');
 const User = require('../models/users.model');
 
 exports.checkSession = async (req, res, next) => {
-    // get token
+    // Get token from headers
     let token;
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
     }
+
     if (!token) {
         console.log('no token');
         return next(new MyError(401, messages.auth.invalidToken.sp));
     }
-    //   validate token
+
+    // Validate the token
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET).catch(err => {
-        return next(new MyError(401, messages.auth.invalidToken.sp))
-    })
+        return next(new MyError(401, messages.auth.invalidToken.sp));
+    });
+
     if (!decoded) {
         console.log('no decoded');
         return next(new MyError(401, messages.auth.invalidToken.sp));
     }
 
-    //   check if user exists
+    // Check if the user still exists
     const freshUser = await User.query().modify('getUserById', decoded.sUserId);
-
     if (!freshUser) {
         console.log('no user !freshUser');
-        return next(new MyError(401), messages.auth.login.notFoundUser.sp);
+        return next(new MyError(401, messages.auth.login.notFoundUser.sp));
     }
 
     req.user = freshUser;
