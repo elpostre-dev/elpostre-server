127827bec54ec048371ccff6b822e3904f52c7b9
Francisco Guerra <paco.gq.0709@gmail.com>
2024-09-25 19:05:36 -0600

    fix cors error 15

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ var orderProductsRouter = require('./routes/orderProducts.routes');
 var app = express();
 
 // Configuración de CORS
-const corsOptions = {
+app.use(cors({
   origin: 'https://elpostrepedidos.netlify.app',
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
-};
-
-app.options('*', cors(corsOptions));
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 // Body parser, reading data from body into req.body
 app.use(express.json());
