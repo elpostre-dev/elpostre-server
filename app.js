8c23b93d694612ba899deb7c3e0a4d320d875e17
Francisco Guerra <paco.gq.0709@gmail.com>
2024-09-25 22:09:06 -0600

    CORS update 2

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ var orderProductsRouter = require('./routes/orderProducts.routes');
 
 var app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://elpostrepedidos.netlify.app',
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+}))
 
 // Body parser, reading data from body into req.body
 app.use(express.json());
